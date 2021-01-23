1aad2eb7307f35f613dd02da8f45c89d95e6f789
Jinyeob <jinyeob07@gmail.com>
2021-01-23 12:33:14 +0900

    update

--- main.py
+++ main.py
@@ -2,65 +2,43 @@ from selenium import webdriver
 import json
 
 url = "http://bible4u.pe.kr/zbxe/?mid=open_read&now_day="
-
-dayofmonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-file_path="./mc.json"
-data={}
-
+loc = '&b_num='
 
 driver = webdriver.Chrome('./chromedriver')
 driver.implicitly_wait(1)
-#
-# for i in range(1, 13):
-#     for j in range(1, dayofmonth[i-1]+1):
-#         loc = '&b_num='
-#
-#         date = str(i) + '-' + str(j)
-#         data[date]=[]
-#
-#         for k in range(1, 5):
-#             data[date] = []
-#
-#             completeUrl = url + date + loc + str(k)
-#             driver.get(completeUrl)
-#
-#             trs = driver.find_elements_by_class_name("li_f_size")
-#             for tr in trs:
-#                 tds = tr.find_elements_by_tag_name("td")
-#                 data[date].append({
-#                     "id": str(k),
-#                     "book": tds[0],
-#                     "verse": tds[1],
-#                     "content": tds[2]
-#                 })
-loc = '&b_num='
 
-date = str(1) + '-' + str(1)
-data[date]=[]
+dayOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-for k in range(1, 5):
-    data[date] = []
 
-    completeUrl = url + date + loc + str(k) #http://bible4u.pe.kr/zbxe/?mid=open_read&now_day=1-1&b_num=1
-    driver.get(completeUrl)
+file_path = "./mcc.json"
+data = {}
 
-    trs = driver.find_elements_by_class_name("li_f_size")
-    for tr in trs:
-        tds = tr.find_elements_by_tag_name("td")
+for i in range(1, 12 + 1):
 
-        print('id: ' + str(k))
-        print('book: ' + tds[0].text)
-        print('verse :' + tds[1].text)
-        print('content: ' + tds[2].text)
+    for j in range(1, dayOfMonth[i-1] + 1):
+        date = str(i) + '-' + str(j)
+        data[date] = []
 
-        data[date].append({
-            "id": str(k),
-            "book": str(tds[0].text),
-            "verse": str(tds[1].text),
-            "content": str(tds[2].text)
-        })
+        for k in range(1, 4 + 1):
+            # http://bible4u.pe.kr/zbxe/?mid=open_read&now_day=1-1&b_num=1
+            completeUrl = url + date + loc + str(k)
+            driver.get(completeUrl)
 
-with open(file_path, 'w') as outfile:
-    json.dump(data, outfile, indent=4)
+            trs = driver.find_elements_by_class_name("li_f_size")
+            for tr in trs:
+                tds = tr.find_elements_by_tag_name("td")
 
+                print('id: ' + str(k))
+                print('book: ' + tds[0].text)
+                print('verse :' + tds[1].text)
+                print('content: ' + tds[2].text)
+
+                data[date].append({
+                    "id": k,
+                    "book": tds[0].text,
+                    "verse": tds[1].text,
+                    "content": tds[2].text
+                })
+
+with open(file_path, 'w', encoding='utf-8') as outfile:
+    json.dump(data, outfile, indent=4, ensure_ascii=False)
